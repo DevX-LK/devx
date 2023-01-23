ec9e5f9ffab91bde7fdf115caa8eb26eb9919682
OshadhaShiro <shirokumara49@gmail.com>
2023-01-23 15:04:52 +0530

    Add ColorSchemeToggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import { HeaderBar } from '@/components/Header';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import {
+	MantineProvider,
+	ColorSchemeProvider,
+	ColorScheme,
+} from '@mantine/core';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+	return (
+		<>
+			<ColorSchemeProvider
+				colorScheme={colorScheme}
+				toggleColorScheme={toggleColorScheme}
+			>
+				<MantineProvider
+					theme={{ colorScheme }}
+					withGlobalStyles
+					withNormalizeCSS
+				>
+					<HeaderBar
+						links={[
+							{ label: 'Home', link: '/' },
+							{ label: 'Projects', link: '/Projects' },
+							{ label: 'Team', link: '/Team' },
+							{ label: 'Contact', link: '/Contact' },
+						]}
+					/>
+					<Component {...pageProps} />
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</>
+	);
 }
