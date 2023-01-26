12f6021ce1f402b203997592864654109deb838a
OshadhaShiro <shirokumara49@gmail.com>
2023-01-26 21:58:54 +0530

    Create Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,22 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
+import { createStyles } from '@mantine/core';
 
-export default function Home() {
-	return <h1>Home</h1>;
-}
+const useStyles = createStyles({
+	container: {
+		paddingTop: '8rem',
+	},
+});
+
+const Home = () => {
+	const { classes, cx } = useStyles();
+
+	return (
+		<div className={classes.container}>
+			<h1>Home</h1>
+		</div>
+	);
+};
+
+export default Home;
