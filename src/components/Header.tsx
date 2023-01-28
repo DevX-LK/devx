fe30ca2d034e0cfc4393c6751e83a81c6db4a478
OshadhaShiro <shirokumara49@gmail.com>
2023-01-28 11:40:04 +0530

    Complete Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,94 @@
-import { useState } from 'react';
-import { createStyles, Header, Container, Group, Burger } from '@mantine/core';
+import {
+	createStyles,
+	Header,
+	Menu,
+	Group,
+	Center,
+	Burger,
+	Container,
+	Transition,
+	Paper,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import Logo from '../assets/logo.png';
+import { IconChevronDown } from '@tabler/icons';
 import Image from 'next/image';
-import { ActionToggle } from './ActionToggle';
+import Link from 'next/link';
+import { useState } from 'react';
+import Logo from '../assets/logo.png';
 
 const useStyles = createStyles((theme) => ({
 	header: {
+		backgroundColor: '#2F9834',
+		color: theme.primaryColor,
+		borderBottom: 0,
+		position: 'absolute',
+		right: '0',
+		left: '0',
+	},
+	dropdown: {
+		position: 'absolute',
+		top: 60,
+		left: 0,
+		right: 0,
+		zIndex: 0,
+		borderTopRightRadius: 0,
+		borderTopLeftRadius: 0,
+		borderTopWidth: 0,
+		overflow: 'hidden',
+
+		[theme.fn.largerThan('md')]: {
+			display: 'none',
+		},
+	},
+
+	inner: {
+		height: 56,
 		display: 'flex',
 		justifyContent: 'space-between',
 		alignItems: 'center',
-		height: '100%',
-		background: '#111',
-		position: 'absolute',
-		width: '100%',
-		right: '0 ',
-		left: '0',
 	},
 
 	links: {
-		[theme.fn.smallerThan('xs')]: {
+		[theme.fn.smallerThan('sm')]: {
 			display: 'none',
 		},
 	},
 
 	burger: {
-		[theme.fn.largerThan('xs')]: {
+		[theme.fn.largerThan('sm')]: {
 			display: 'none',
 		},
 	},
 
 	link: {
+		display: 'block',
+		lineHeight: 1,
+		padding: '8px 12px',
+		borderRadius: theme.radius.sm,
+		textDecoration: 'none',
+		color: theme.white,
+		fontSize: theme.fontSizes.sm,
+		fontWeight: 500,
+
+		'&:hover': {
+			backgroundColor: 'rgba(5, 5,5, 0.2)',
+			color: '#fff',
+		},
+		[theme.fn.smallerThan('sm')]: {
+			borderRadius: 0,
+			padding: theme.spacing.md,
+			'&:hover': {
+				backgroundColor: 'rgba(47, 152, 52, 0.2)',
+				color: 'rgb(47, 152, 52)',
+			},
+		},
+	},
+
+	linkLabel: {
+		marginRight: 5,
+	},
+
+	linkActive: {
 		display: 'block',
 		lineHeight: 1,
 		padding: '8px 12px',
@@ -42,75 +100,91 @@ const useStyles = createStyles((theme) => ({
 				: theme.colors.gray[7],
 		fontSize: theme.fontSizes.sm,
 		fontWeight: 500,
-
-		'&:hover': {
-			backgroundColor:
-				theme.colorScheme === 'dark'
-					? theme.colors.dark[6]
-					: theme.colors.gray[0],
-		},
-	},
-
-	linkActive: {
 		'&, &:hover': {
-			backgroundColor: theme.fn.variant({
-				variant: 'light',
-				color: theme.primaryColor,
-			}).background,
-			color: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-				.color,
+			backgroundColor: 'rgba(5, 5, 5, 0.4)',
+			color: '#fff',
+		},
+		[theme.fn.smallerThan('sm')]: {
+			'&, &:hover': {
+				backgroundColor: 'rgba(47, 152, 52, 0.2)',
+				color: 'rgb(47, 152, 52)',
+			},
 		},
 	},
 }));
 
-interface HeaderBarProps {
-	links: { link: string; label: string }[];
+interface HeaderSearchProps {
+	links: {
+		link: string;
+		label: string;
+	}[];
 }
 
-export const HeaderBar = ({ links }: HeaderBarProps) => {
+export function HeaderBar({ links }: HeaderSearchProps) {
 	const [opened, { toggle }] = useDisclosure(false);
-	const [active, setActive] = useState(links[0].link);
-	const { classes, cx } = useStyles();
+	const [active, setActive] = useState(links[0].label);
+	const { classes } = useStyles();
 
-	const items = links.map((link) => (
-		<a
-			key={link.label}
-			href={link.link}
-			className={cx(classes.link, {
-				[classes.linkActive]: active === link.link,
-			})}
-			onClick={(event) => {
-				event.preventDefault();
-				setActive(link.link);
-			}}
-		>
-			{link.label}
-		</a>
-	));
+	const items = links.map((link) => {
+		return (
+			<a
+				key={link.label}
+				href={link.link}
+				className={active === link.label ? classes.linkActive : classes.link}
+				onClick={(event) => {
+					event.preventDefault();
+					setActive(link.label);
+				}}
+			>
+				{link.label}
+			</a>
+		);
+	});
 
 	return (
-		<Header
-			height={70}
-			mb={40}
-			sx={{
-				position: 'absolute',
-				right: '0',
-				left: '0',
-			}}
-		>
-			<Container className={classes.header}>
-				<Image src={Logo} alt="DevX Logo" width={102} />
-				<Group spacing={5} className={classes.links}>
-					{items}
-				</Group>
-
-				<Burger
-					opened={opened}
-					onClick={toggle}
-					className={classes.burger}
-					size="sm"
-				/>
+		<Header height={56} className={classes.header} mb={120}>
+			<Container>
+				<div className={classes.inner}>
+					<Image src={Logo} alt="DevX logo" width={102} />
+					<Group spacing={5} className={classes.links}>
+						{items}
+					</Group>
+					<Burger
+						opened={opened}
+						onClick={toggle}
+						className={classes.burger}
+						size="sm"
+						color="#fff"
+					/>
+				</div>
 			</Container>
+			<Transition transition="pop-top-right" duration={200} mounted={opened}>
+				{(styles) => (
+					<Paper
+						key={Math.random()}
+						className={classes.dropdown}
+						withBorder
+						style={styles}
+					>
+						{links.map((link) => (
+							<Link href={link.link} legacyBehavior key={link.label}>
+								<a
+									key={link.label}
+									className={
+										active === link.label ? classes.linkActive : classes.link
+									}
+									onClick={(event) => {
+										setActive(link.label);
+										close();
+									}}
+								>
+									{link.label}
+								</a>
+							</Link>
+						))}
+					</Paper>
+				)}
+			</Transition>
 		</Header>
 	);
-};
+}
